b2843448d97d1e17fff258b9f214dcdc82e9b740
Maxwell DeMaio <maxdman321@gmail.com>
2020-12-17 11:40:13 -0500

    Started testing bar graph

--- py/duo.py
+++ py/duo.py
@@ -1,4 +1,22 @@
+import os
+import json
 import duolingo
-from ..import config
+from dotenv import load_dotenv
 
-SECRET_KEY = os.environ.get("DUO_PASS")
+# Load env variables
+load_dotenv()
+DUO_PASS = os.environ.get("DUO_PASS")
+
+# All information from request
+lingo = duolingo.Duolingo('maxwelldemaio', DUO_PASS)
+
+# Get array of languages I am learning
+myLangs = lingo.get_languages()
+
+# Get all language information
+myLangsInfo = []
+for lang in myLangs:
+    myLangsInfo.append(lingo.get_language_details(lang))
+
+# Create JSON of language info
+print(json.dumps(myLangsInfo))
